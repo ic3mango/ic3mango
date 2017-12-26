669310a96f8cb8e90265eac85d88f024373be384
Stefan <stefantrysil@gmail.com>
2017-12-26 23:19:40 +0100

    add styled-components

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components';
+
+const Intro = styled.h1`
+  font-size: 32px;
+  margin-bottom: 100px;
+`
 
 const SecondPage = ({ transition }) => (
   <div style={transition && transition.style}>
-    <h1>Hi from the second page</h1>
+    <Intro>Hi from the second page</Intro>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>

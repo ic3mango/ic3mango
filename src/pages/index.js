71a2978158cd9255fd833eb1928dde5d0fe5c2cb
Stefan <stefantrysil@gmail.com>
2018-01-15 21:11:40 +0100

    fix invalid html

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ const IndexPage = ({ transition }) => (
   <div style={transition && transition.style}>
     <Inner >
       <h1>Stefan's Portfolio</h1>
-      <p>Liberty, when it begin to take root, is a plant of rapid growth.
-        <p>- George Washington</p>
-      </p>
+      <p>Liberty, when it begin to take root, is a plant of rapid growth.</p>
+      <p>- George Washington</p>
       <ButtonDefault to="/about">Find out more</ButtonDefault>
     </Inner>
   </div>
